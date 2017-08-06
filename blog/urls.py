46eb5f576a5690b5c9a80fdbadab5723deaa251c
Benjamin <benjamin.scheinhardt@gmail.com>
2017-08-06 20:36:25 +0200

    clean + link portfolio qui marche

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^accueil$', views.accueil),
+    url(r'^accueil/$', views.accueil, name="accueil"),
     url(r'^contact/$', views.contact, name="contact"),
     url(r'^article/(?P<id_article>\d+)-(?P<slug>.+)$', views.read_article, name="read_article"),
 ]
